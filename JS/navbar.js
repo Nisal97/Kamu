e310f01ba8a9c517d0bc2de8f8042fa687d22e0a
Dimalsha <dimalsha.manawadu@gmail.com>
2019-12-18 09:14:59 +0530

    Added menu

--- JS/navbar.js
+++ JS/navbar.js
@@ -3,4 +3,25 @@ jQuery(function ($) {
         $('.menu').toggleClass('right');
         $('.toggle').toggleClass('indexcity');
     });
+    
+    $('#menu').height($(document).height());
+
+    $('#menu').click(function(event){
+        event.stopPropagation();
+    });
+    $('.toggle').click(function(event){
+        event.stopPropagation();
+    });
+
+    $(window).click(openCloseMenu);
 });
+
+function screenSize() {
+    console.log('HEIGHT - ', $(document).height());
+    document.getElementById('menu').style.height = $(document).height();
+}
+
+function openCloseMenu() {
+    $('.menu').toggleClass('right');
+    $('.toggle').toggleClass('indexcity');
+}
