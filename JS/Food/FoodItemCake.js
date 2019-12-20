cf4bd3bb5f1518b27fdeffd9503f0b406a61723f
Charana Mayakaduwa <charana2010@gmail.com>
2019-12-21 00:03:53 +0530

    Added search bar

--- JS/Food/FoodItemCake.js
+++ JS/Food/FoodItemCake.js
@@ -42,3 +42,39 @@ var foodList = {
         "description": "A velvety surprise, round shaped red velvet sponge made with butter, sugar, milk, orange juice, flour and coco powder. Layered and covered with cream cheese and strawberry garnishes."
     }
 }
+
+var input = document.getElementById("searchInput");
+if(input){
+    console.log("searchInput is available");
+    input.addEventListener("input", myFunction);
+} else{
+    console.log("searchInput is null");
+}
+
+function myFunction(e) {
+    var filter = e.target.value.toUpperCase();
+    console.log("Came to myFunction(e)");
+
+    var list = document.getElementById("cakeList");
+    var divs = list.getElementsByTagName("div");
+    for (var i = 0; i < divs.length; i++) {
+        console.log("divs.length - "+ divs.length);
+        var a = divs[i].getElementsByTagName("h3")[0];
+        var b = divs[i].getElementsByTagName("h3")[1];
+        if (a) {
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                divs[i].style.display = "";
+            } else {
+                divs[i].style.display = "none";
+            }
+        }
+        if (b){
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -2) {
+                divs[i].style.display = "";
+            } else {
+                divs[i].style.display = "none";
+            }
+        }
+    }
+
+}
